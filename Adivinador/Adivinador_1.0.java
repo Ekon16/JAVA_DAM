a3eff8b0587103f8e8bf3a2c0e2d0a4c29bcf8f5
Ekon16 <64109075+Ekon16@users.noreply.github.com>
2021-10-21 21:45:21 +0200

    Create Adivinador_1.0.java

---
+++ Adivinador/Adivinador_1.0.java
@@ -0,0 +1,71 @@
+package Adivina;
+
+import java.util.Scanner;
+
+public class Adivinador2 {
+	public static void main(String[] args) 
+	{
+
+		Scanner teclado = new Scanner(System.in);
+
+		
+		// Variables 
+		int min = 0;
+		int max = 100;
+		int num1;
+		char rep = 'y';
+
+		System.out.println("Piensa un puto numero del " + min + " al " + max);
+		
+		while(rep =='Y' || rep == 'y')
+		{
+				
+			if (min == max)
+			{
+				num1 = min;
+			}
+			else 
+			{
+				// Empieza a pensar
+				num1 = (max+min)/2;
+			}
+			
+			if (num1 <= min || num1 >= max) {
+				System.out.println("La chupas por tramposo");
+				break;
+			}
+			System.out.println("Que te parece el " + num1 + " (<,=,>)? " );
+			
+			// -------------------------
+		
+			String input = teclado.nextLine();
+			
+			// ------------------------------
+			if(input.equals(">"))
+			{
+				// si es mayor se cambia el minimo
+				min = num1;
+			}
+			else if (input.equals("<"))
+			{
+				// Si es menor
+				max = num1;
+			}
+			else if (input.equals("="))
+			{
+				System.out.println("tu numero es " + num1);
+				System.out.println("Buena partida GG :v");
+				System.out.println("Echamos otra partida (y,n)?");		
+			}
+			String second = teclado.nextLine();
+			
+			if (second.equals('y'))
+				{
+					rep = input.charAt(0);
+					
+					rep = 'y';		
+						
+				} 
+			}
+		}
+	}
